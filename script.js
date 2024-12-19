c42b17b6ac5610a05de4b3715ef131468335bc94
nailovan <nelanailova@gmail.com>
2024-12-19 23:57:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Tampilkan pesan di console browser
+console.log('Halo, ini website profilku!');
+
+// Contoh interaksi: alert saat halaman selesai dimuat
+window.onload = function () {
+    alert('Welcome to my profile!');
+};
